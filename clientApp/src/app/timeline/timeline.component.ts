e930aee975515a2738872a5111670619e49f5525
Kash <keshpc1245@gmail.com>
2017-12-29 12:23:48 -0500

    Update

--- clientApp/src/app/timeline/timeline.component.ts
+++ clientApp/src/app/timeline/timeline.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  AfterViewInit,
+  OnDestroy,
+  ViewChild
+} from '@angular/core';
+
+import { FormControl } from '@angular/forms';
+import 'highcharts/adapters/standalone-framework.src';
+
+const Highcharts = require('highcharts/highcharts.src');
 
 @Component({
   selector: 'timeline',
   templateUrl: './timeline.component.html',
   styleUrls: ['./timeline.component.css']
 })
-export class TimelineComponent implements OnInit {
+export class TimelineComponent implements AfterViewInit, OnDestroy {
+  curriculum = new FormControl();
+  focus = new FormControl();
+  location = new FormControl();
+  bulding = new FormControl();
 
-  constructor() { }
+  curriculumList = ['Java', '.NET', 'SDET', 'HIBERNATE', 'SPRING', 'BIG DATA'];
+  focusList = ['Java', '.NET', 'SDET', 'HIBERNATE', 'SPRING', 'BIG DATA'];
+  locationList = ['Java', '.NET', 'SDET', 'HIBERNATE', 'SPRING', 'BIG DATA'];
+  buldingList = ['Java', '.NET', 'SDET', 'HIBERNATE', 'SPRING', 'BIG DATA'];
 
-  ngOnInit() {
+  @ViewChild('chart') public chartEl: ElementRef;
+
+  private _chart: any;
+
+  public ngAfterViewInit() {
+    let opts: any = {
+      title: {
+        text: 'Batches',
+        x: -20 //center
+      },
+      xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+      },
+      series: [{
+        name: 'Weeks',
+        data: [
+         5, 10, 15
+        ]
+      }]
+        
+    };
+
+    if (this.chartEl && this.chartEl.nativeElement) {
+      opts.chart = {
+        type: 'line',
+        renderTo: this.chartEl.nativeElement
+      };
+
+      this._chart = new Highcharts.Chart(opts);
+    }
   }
 
+  public ngOnDestroy() {
+    this._chart.destroy();
+  }
 }
