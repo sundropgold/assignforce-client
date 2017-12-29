89eaea356d98229682a69f29b3242747301c000d
junli1014 <1014gin@gmail.com>
2017-12-29 11:49:37 -0500

    delete unnecessary lines

--- clientApp/src/app/curricula/curricula.component.ts
+++ clientApp/src/app/curricula/curricula.component.ts
@@ -153,7 +153,6 @@ export class CurriculaComponent implements OnInit {
         width: '250px',
         data: curriculum
       });
-    dialogRef.componentInstance.
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('create-focus dialog closed');
