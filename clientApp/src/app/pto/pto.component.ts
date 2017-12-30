98785f3e2892663d849dd33b0fce4c49c6da0515
Kensen Alexandre <kalexandre13@gmail.com>
2017-12-29 20:55:38 -0500

    Added Pto Calendar

--- clientApp/src/app/pto/pto.component.ts
+++ clientApp/src/app/pto/pto.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 
 @Component({
   selector: 'app-pto',
@@ -13,3 +14,23 @@ export class PtoComponent implements OnInit {
   }
 
 }
+
+//Used for the display of new Trainer dialog box
+@Component({
+  selector: 'app-calendar-dialog',
+  templateUrl: './calendar-dialog.component.html',
+})
+export class CalendarDialogComponent {
+
+  constructor(public dialogRef: MatDialogRef<CalendarDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any) {
+  }
+
+  onNoClick(): void {
+    event.stopPropagation();
+    this.dialogRef.close();
+  }
+
+
+}
+
