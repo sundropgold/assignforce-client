675b587bc0d48ef378a98796f6cc5f65ad5896bc
kukuforever <kanxue0012@gmail.com>
2018-01-08 10:13:08 -0500

    update style

--- clientApp/src/app/services/user-info.service.ts
+++ clientApp/src/app/services/user-info.service.ts
@@ -7,26 +7,11 @@ import {Observable} from 'rxjs/Observable';
 @Injectable()
 export class UserInfoService {
 
-<<<<<<< HEAD
-    user: User
-
-    constructor(private url: UrlService,
-		private http: HttpClient) { }
-=======
   user: User;
->>>>>>> 509d8d12e6b6b446070dae4f46c0e636ce3664b7
 
   constructor(private url: UrlService,
               private http: HttpClient) { }
 
-<<<<<<< HEAD
-    getUser(){
-	return this.user;
-    }
-    setUser(u: User){
-	this.user = u;
-    }
-=======
   loadUser(): Observable<User> {
     return this.http.get<User>(this.url.getUrl() + '/auth/userinfo');
   }
@@ -37,5 +22,4 @@ export class UserInfoService {
   setUser(u: User) {
     this.user = u;
   }
->>>>>>> 509d8d12e6b6b446070dae4f46c0e636ce3664b7
 }
