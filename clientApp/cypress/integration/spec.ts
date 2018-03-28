73dd796c372aca5bc4dbded489fedd02a2f0c6d4
Robert Hamel <roberthamel@gmail.com>
2018-03-28 09:25:41 -0400

    implement snapshot testing

--- clientApp/cypress/integration/spec.ts
+++ clientApp/cypress/integration/spec.ts
@@ -1,3 +1,8 @@
 it('loads', () => {
   cy.visit('http://localhost:4200');
 });
+
+it('takes a snapshot', () => {
+  cy.visit('http://localhost:4200');
+  cy.get('p').snapshot({name: 'basic login page'});
+});
