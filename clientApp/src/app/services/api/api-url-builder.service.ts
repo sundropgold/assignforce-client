f52bbb2176f64c6aa10b16242ab2ccb290bc4cd7
Sawyer Novak <sawyern@me.com>
2018-03-29 13:22:58 -0400

    added setting model

--- clientApp/src/app/services/api/api-url-builder.service.ts
+++ clientApp/src/app/services/api/api-url-builder.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@angular/core';
 import { GetApiUrlService } from './getApiUrl.service';
+import { Setting } from '../../model/setting';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ApiUrlBuilderService {
-  constructor(private apiUrlService: GetApiUrlService) {}
+  constructor(private apiUrlService: GetApiUrlService, private http: HttpClient) {}
+
+  // private settingsUrl : string = '/api/v2/setting';
+  //
+  // retrieveSetting(settingId : number) : Observable<Setting> {
+  //   return this.http.get<Setting>(`${this.settingsUrl}/${settingId}`)
+  // }
+  //
+  //
 }
