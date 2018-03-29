fc6117b905581acd69cf56526d4659c12f532bcb
Justin2375 <justin.jarrard@siu.edu>
2018-03-29 14:37:08 -0400

    name updating complete

--- clientApp/src/app/components/profile/profile.component.ts
+++ clientApp/src/app/components/profile/profile.component.ts
@@ -4,6 +4,7 @@ import { TrainerService } from '../../services/trainer/trainer.service';
 import { S3CredentialService } from '../../services/s3-credential/s3-credential.service';
 import { Trainer } from '../../model/trainer';
 import { Skill } from '../../model/skill';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -13,8 +14,14 @@ import { Skill } from '../../model/skill';
 export class ProfileComponent implements OnInit {
   @Input() fName: string;
   @Input() lName: string;
+
   tId: -1;
   lockProfile: true;
+  fb: FormBuilder = new FormBuilder();
+  nameForm = this.fb.group({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required)
+  });
 
   // data
   skills: Skill[] = [
@@ -23,6 +30,7 @@ export class ProfileComponent implements OnInit {
     { skillId: 3, name: 'Angular', active: true },
     { skillId: 4, name: 'C++', active: true }
   ];
+  nameFound = false;
   myFile: FileList;
   creds: any;
   certFile: FileList = null;
@@ -115,6 +123,15 @@ export class ProfileComponent implements OnInit {
     //
     //   // set my file to undefined so that update and label will be hidden in the html
     this.myFile = undefined;
+  }
+
+  //Updates user's name
+  updateName() {
+    console.log(this.nameForm.value.firstName);
+
+    this.nameFound = true;
+    this.trainer.firstName = this.nameForm.value.firstName;
+    this.trainer.lastName = this.nameForm.value.lastName;
   }
 
   // called to save the current state of the trainers skills
