5a34ae5b8db3666f0efe10302082fce6123464af
flynntan <flynnigan.tan@gmail.com>
2018-03-28 21:59:48 -0400

    update package json

--- clientApp/src/app/components/overview/overview.component.spec.ts
+++ clientApp/src/app/components/overview/overview.component.spec.ts
@@ -1,13 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OverviewComponent } from './overview.component';
-<<<<<<< HEAD
-import {AppMaterialModule} from "../../app-material/app-material.module";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-=======
 import { AppMaterialModule } from '../../material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
->>>>>>> 78ee2b49f10141e7105c5664ab20c99f06e926a9
 
 describe('OverviewComponent', () => {
   let component: OverviewComponent;
