6ada59902384e03dc117563c3a7aaf77959acdf9
Sawyer Novak <sawyern@me.com>
2018-03-30 14:17:16 -0400

    added address urls

--- clientApp/src/app/services/api/api-url-builder.service.ts
+++ clientApp/src/app/services/api/api-url-builder.service.ts
@@ -8,11 +8,41 @@ import { HttpClient } from '@angular/common/http';
 export class ApiUrlBuilderService {
   constructor(private apiUrlService: GetApiUrlService, private http: HttpClient) {}
 
-  // private settingsUrl : string = '/api/v2/setting';
-  //
-  // retrieveSetting(settingId : number) : Observable<Setting> {
-  //   return this.http.get<Setting>(`${this.settingsUrl}/${settingId}`)
-  // }
-  //
-  //
+  private baseUrl = 'https://assignforceback.cfapps.io';
+
+  private addressControllerApi = {
+    baseUrl: '/address',
+    create: '/vp/location/create',
+    update: '/vp/location/update',
+    remove: '/vp/location/delete',
+    all: '/all/location/all',
+    reactivate: '/vp/location/reactivate'
+  };
+
+  private buildingControllerApi = {
+    baseUrl: '/api/v2/building',
+    create: '/',
+    retrieve: '/',
+    update: '/',
+    remove: '/',
+    all: '/'
+  };
+
+  private locationControllerApi = {
+    baseUrl: '/api/v2/location',
+    create: '/',
+    retrieve: '/',
+    update: '/',
+    remove: '/',
+    all: '/'
+  };
+
+  private roomControllerApi = {
+    baseUrl: '/api/v2/room',
+    create: '/',
+    retrieve: '/',
+    update: '/',
+    remove: '/',
+    all: '/'
+  };
 }
