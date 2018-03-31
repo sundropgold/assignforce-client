a01893be5637f80f15c72e375a5c14fa470b4e5d
Paul Fox <pfoxiii@gmail.com>
2018-03-31 09:23:14 -0400

    Readded more tests since I figured my problems out

--- clientApp/src/app/components/core/core.component.spec.ts
+++ clientApp/src/app/components/core/core.component.spec.ts
@@ -11,12 +11,14 @@ import { Curriculum } from '../../model/Curriculum';
 describe('CoreComponent', () => {
   let component: CoreComponent;
   let fixture: ComponentFixture<CoreComponent>;
-  const testData: Curriculum = new Curriculum(1, 'Test Curriculum', true, true, [
-    { skillId: 1, name: 'Test Skill', active: true }
-  ]);
+  const testData: Curriculum[] = [
+    new Curriculum(1, 'Test Curriculum', true, true, [{ skillId: 1, name: 'Test Skill', active: true }]),
+    new Curriculum(2, 'Test Curriculum 2', true, true, [{ skillId: 1, name: 'Test Skill', active: true }])
+  ];
+  let curriculaService = CurriculaService;
 
   class MockCurriculaService {
-    getCurricula(): Observable<Curriculum> {
+    getCurricula(): Observable<Curriculum[]> {
       return Observable.of(testData);
     }
   }
@@ -28,6 +30,7 @@ describe('CoreComponent', () => {
         declarations: [CoreComponent],
         providers: [{ provide: CurriculaService, useClass: MockCurriculaService }]
       }).compileComponents();
+      curriculaService = TestBed.get(CurriculaService);
     })
   );
 
@@ -38,32 +41,32 @@ describe('CoreComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-  // it('should contain a mat-list', () => {
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('mat-list')).toBeTruthy();
-  // });
+  it('should contain a mat-list', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('mat-list')).toBeTruthy();
+  });
 
-  // it('should contain the core name', () => {
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('.core-name').textContent).toContain(component.coreData[0].name);
-  // });
+  it('should contain the core name', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.core-name').textContent).toContain(component.coreData[0].name);
+  });
 
-  // it('should contain the skills for the core', () => {
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('.core-skills').textContent).toContain(component.coreData[0].skills[0]);
-  // });
+  it('should contain the skills for the core', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.core-skills').textContent).toContain(component.coreData[0].skills[0].name);
+  });
 
-  // it('should contain an accordion for core curriculum', () => {
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('mat-accordion')).toBeTruthy();
-  // });
+  it('should contain an accordion for core curriculum', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('mat-accordion')).toBeTruthy();
+  });
 
-  // it('should contain a title named Core Curricula', () => {
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('mat-panel-title').textContent).toContain('Core Curricula');
-  // });
+  it('should contain a title named Core Curricula', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('mat-panel-title').textContent).toContain('Core Curricula');
+  });
 });
