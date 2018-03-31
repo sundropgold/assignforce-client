cc0dee7b27de431468f15b75fbf2ccedb50dc3dc
Sawyer Novak <sawyern@me.com>
2018-03-30 22:03:12 -0400

    Merge branches 'jasonSong' and 'staging' of https://github.com/sawyern/assignforce-client into jasonSong
    # Conflicts:
    #	.idea/workspace.xml
    #	clientApp/src/app/app.component.spec.ts
    #	clientApp/src/app/app.module.ts
    #	clientApp/src/app/components/batches/batches.component.spec.ts
    #	clientApp/src/app/components/batches/batches.component.ts
    #	clientApp/src/app/components/curricula/curricula.component.spec.ts
    #	clientApp/src/app/components/curricula/curricula.component.ts
    #	clientApp/src/app/components/locations/locations.component.spec.ts
    #	clientApp/src/app/components/menu-bar/menu-bar.component.html
    #	clientApp/src/app/components/menu-bar/menu-bar.component.spec.ts
    #	clientApp/src/app/components/overview/overview.component.spec.ts
    #	clientApp/src/app/components/profile/profile.component.spec.ts
    #	clientApp/src/app/components/reports/reports.component.spec.ts
    #	clientApp/src/app/components/settings/settings.component.spec.ts
    #	clientApp/src/app/components/trainers/trainers.component.html
    #	clientApp/src/app/components/trainers/trainers.component.spec.ts
    #	package-lock.json

--- clientApp/src/app/components/settings/settings.component.spec.ts
+++ clientApp/src/app/components/settings/settings.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing'
 
 import { SettingsComponent } from './settings.component';
 import { FormsModule } from '@angular/forms';
-import { MatExpansionModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatExpansionModule, MatFormFieldModule, MatInputModule, MatGridListModule } from '@angular/material';
 import { HttpClient } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -14,7 +14,14 @@ describe('SettingsComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [BrowserAnimationsModule, FormsModule, MatFormFieldModule, MatInputModule, MatExpansionModule],
+      imports: [
+        BrowserAnimationsModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatExpansionModule,
+        MatGridListModule
+      ],
       declarations: [SettingsComponent],
       providers: [{ provide: HttpClient, useValue: mockClient }]
     }).compileComponents();
@@ -28,6 +35,4 @@ describe('SettingsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  // it('should ')
 });
