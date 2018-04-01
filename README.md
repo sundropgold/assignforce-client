d63d1a4bfa6d53f07e2256b488e6b049ff7b93f5
pdawson2090 <pdawson2090@gmail.com>
2018-04-01 16:51:47 -0400

    folder manuevering

--- README.md
+++ README.md
@@ -1,41 +1,27 @@
-# assignforce-client
+# Project1Front
 
-## Syncing of Upstream forks
-Makes it a little bit easier to sync a forked repository with its upstream counterpart.
-> This tool is compatible with Linux, MacOS, and Windows Subsystem Linux only.
-1. Change to `clientApp` directory
-```bash
-cd clientApp
-```
-2. To view all commands
-```bash
-sh fsync
-```
-You should see the following:
-```bash
-sh fsync init    --> initialize upstream and sync
-sh fsync sync    --> sync with upstream
-sh fsync merge   --> sync with upstream, push and merge with current branch (use with caution)
-```
-3. `fsync init`
-Use this option when this is your first time syncing your fork with the parent repository.
-```bash
-sh fsync init
-```
-4. `fsync sync`
-Use this option each time aftwards.
-```bash
-sh fsync sync
-```
-This option will _not_ push the new changes to GitHub... Use option 5 for that.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.5.0.
 
-5. `fsync merge`
-If you're feeling confident, this command will fetch from upstream, commit to the forked repository, switch to the branch you were working on, and merge staging into your branch. (use carefully)
-```bash
-sh fsync merge
-```
-> The `sync` and `merge` commands will stash any uncommitted files, and then unstash them when it completes its task.
+## Development server
 
----
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-Angular client for Assignforce
+## Code scaffolding
+
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `-prod` flag for a production build.
+
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
---
+++ src/app/app.module.ts
@@ -0,0 +1,111 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { SpringXsrfInterceptor } from './interceptors/springXsrfInterceptor';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { OverviewComponent } from './components/overview/overview.component';
+import { BatchesComponent } from './components/batches/batches.component';
+import {
+  // LocationAddBuildingDialogComponent,
+  LocationAddLocationDialogComponent,
+  LocationAddRoomDialogComponent,
+  LocationDeleteBuildingDialogComponent,
+  LocationDeleteLocationDialogComponent,
+  LocationDeleteRoomDialogComponent,
+  LocationEditBuildingDialogComponent,
+  LocationEditLocationDialogComponent,
+  LocationEditRoomDialogComponent,
+  LocationsComponent
+} from './components/locations/locations.component';
+import { LocationAddDialogComponent } from './components/locations/add-dialog/location-add-dialog.component';
+import { CurriculaComponent } from './components/curricula/curricula.component';
+import { TrainersComponent } from './components/trainers/trainers.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ReportsComponent } from './components/reports/reports.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { AppRouting } from './app.routing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TrainerService } from './services/trainer/trainer.service';
+import { SkillService } from './services/skill/skill.service';
+import { S3CredentialService } from './services/s3-credential/s3-credential.service';
+import { UrlService } from './services/url/url.service';
+
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
+import { AppMaterialModule } from './material.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemDbService } from './mockdb/in-mem-db.service';
+
+import { TrainersAddComponent } from './components/trainers/trainers-add/trainers-add.component';
+import { TrainerItemComponent } from './components/trainers/trainer-item/trainer-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuBarComponent,
+    OverviewComponent,
+    BatchesComponent,
+    LocationsComponent,
+    CurriculaComponent,
+    TrainersComponent,
+    ProfileComponent,
+    ReportsComponent,
+    SettingsComponent,
+    LocationAddLocationDialogComponent,
+    LocationDeleteLocationDialogComponent,
+    LocationEditLocationDialogComponent,
+    LocationAddDialogComponent, // LocationAddBuildingDialogComponent,
+    LocationDeleteBuildingDialogComponent,
+    LocationEditBuildingDialogComponent,
+    LocationAddRoomDialogComponent,
+    LocationDeleteRoomDialogComponent,
+    LocationEditRoomDialogComponent,
+    LoginComponent,
+    TrainersAddComponent,
+    TrainerItemComponent
+  ],
+
+  imports: [
+    HttpClientModule,
+    FormsModule,
+    BrowserModule,
+    AppRouting,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    InMemoryWebApiModule.forRoot(InMemDbService)
+  ],
+
+  providers: [
+    TrainerService,
+    SkillService,
+    S3CredentialService,
+    HttpClient,
+    UrlService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: SpringXsrfInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [
+    LocationAddLocationDialogComponent,
+    LocationDeleteLocationDialogComponent,
+    LocationEditLocationDialogComponent,
+    LocationAddDialogComponent, // LocationAddBuildingDialogComponent,
+    LocationDeleteBuildingDialogComponent,
+    LocationEditBuildingDialogComponent,
+    LocationAddRoomDialogComponent,
+    LocationDeleteRoomDialogComponent,
+    LocationEditRoomDialogComponent,
+    TrainersAddComponent
+  ]
+})
+export class AppModule {}
