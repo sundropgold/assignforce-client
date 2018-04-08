e22d3f5ded010dc85d1646da72a21e03563ff0f5
Robert Hamel <roberthamel@gmail.com>
2018-04-08 02:24:40 -0400

    fix broken navbar

--- src/app/mockdb/mockdata/trainer.data.ts
+++ src/app/mockdb/mockdata/trainer.data.ts
@@ -1,250 +1,290 @@
-// import { Trainer } from '../../model/Trainer';
-//
-// export const trainers: Trainer[] = [
-//   {
-//     id: 1,
-//     firstName: 'August',
-//     lastName: 'Duet',
-//     resume: null,
-//     unavailabilities: [
-//       {
-//         startDate: 1502078400000,
-//         endDate: 1507867200000,
-//         id: 317
-//       },
-//       {
-//         startDate: 1517806800000,
-//         endDate: 1523592000000,
-//         id: 555
-//       },
-//       {
-//         startDate: 1508731200000,
-//         endDate: 1515128400000,
-//         id: 412
-//       },
-//       {
-//         startDate: 1495425600000,
-//         endDate: 1501214400000,
-//         id: 217
-//       },
-//       {
-//         startDate: 1494820800000,
-//         endDate: 1501819200000,
-//         id: 48
-//       },
-//       {
-//         startDate: 1494820800000,
-//         endDate: 1501819200000,
-//         id: 46
-//       }
-//     ],
-//     skills: [
-//       {
-//         id: 4,
-//         name: 'AngularJS',
-//         active: true
-//       },
-//       {
-//         id: 1,
-//         name: 'Core Java',
-//         active: true
-//       },
-//       {
-//         id: 2,
-//         name: 'JUnit',
-//         active: true
-//       },
-//       {
-//         id: 3,
-//         name: 'Spring',
-//         active: true
-//       },
-//       {
-//         id: 50,
-//         name: 'Java Servlets',
-//         active: true
-//       },
-//       {
-//         id: 51,
-//         name: 'JSP',
-//         active: true
-//       },
-//       {
-//         id: 48,
-//         name: 'JDBC',
-//         active: true
-//       },
-//       {
-//         id: 49,
-//         name: 'HTML',
-//         active: true
-//       },
-//       {
-//         id: 52,
-//         name: 'Freemarker',
-//         active: true
-//       },
-//       {
-//         id: 53,
-//         name: 'CSS',
-//         active: true
-//       },
-//       {
-//         id: 54,
-//         name: 'jQuery',
-//         active: true
-//       },
-//       {
-//         id: 55,
-//         name: 'Hibernate',
-//         active: true
-//       },
-//       {
-//         id: 56,
-//         name: 'REST',
-//         active: true
-//       },
-//       {
-//         id: 57,
-//         name: 'SOAP',
-//         active: true
-//       },
-//       {
-//         id: 70,
-//         name: 'Python',
-//         active: true
-//       }
-//     ],
-//     certifications: [],
-//     active: true
-//   },
-//   {
-//     id: 2,
-//     firstName: 'Fred',
-//     lastName: 'Belotte',
-//     resume: null,
-//     unavailabilities: [
-//       {
-//         startDate: 1494216000000,
-//         endDate: 1495166400000,
-//         id: 210
-//       },
-//       {
-//         startDate: 1512363600000,
-//         endDate: 1518757200000,
-//         id: 489
-//       },
-//       {
-//         startDate: 1520827200000,
-//         endDate: 1526616000000,
-//         id: 610
-//       },
-//       {
-//         startDate: 1503288000000,
-//         endDate: 1509681600000,
-//         id: 387
-//       },
-//       {
-//         startDate: 1495425600000,
-//         endDate: 1501214400000,
-//         id: 232
-//       }
-//     ],
-//     skills: [],
-//     certifications: [],
-//     active: true
-//   },
-//   {
-//     id: 3,
-//     firstName: 'Joe',
-//     lastName: 'Kirkbride',
-//     resume: null,
-//     unavailabilities: [
-//       {
-//         startDate: 1498449600000,
-//         endDate: 1504238400000,
-//         id: 237
-//       }
-//     ],
-//     skills: [],
-//     certifications: [],
-//     active: false
-//   },
-//   {
-//     id: 11,
-//     firstName: 'Nicholas',
-//     lastName: 'Jurczak',
-//     resume: null,
-//     unavailabilities: [
-//       {
-//         startDate: 1507521600000,
-//         endDate: 1513314000000,
-//         id: 473
-//       },
-//       {
-//         startDate: 1519621200000,
-//         endDate: 1525406400000,
-//         id: 553
-//       },
-//       {
-//         startDate: 1499659200000,
-//         endDate: 1505448000000,
-//         id: 257
-//       }
-//     ],
-//     skills: [],
-//     certifications: [],
-//     active: true
-//   },
-//   {
-//     id: 7,
-//     firstName: 'Yuvi',
-//     lastName: 'Damodaran',
-//     resume: null,
-//     unavailabilities: [
-//       {
-//         startDate: 1522641600000,
-//         endDate: 1528430400000,
-//         id: 615
-//       },
-//       {
-//         startDate: 1499054400000,
-//         endDate: 1504843200000,
-//         id: 272
-//       },
-//       {
-//         startDate: 1494216000000,
-//         endDate: 1501214400000,
-//         id: 32
-//       },
-//       {
-//         startDate: 1493006400000,
-//         endDate: 1495166400000,
-//         id: 67
-//       },
-//       {
-//         startDate: 1512968400000,
-//         endDate: 1519362000000,
-//         id: 492
-//       },
-//       {
-//         startDate: 1505102400000,
-//         endDate: 1510894800000,
-//         id: 527
-//       },
-//       {
-//         startDate: 1494216000000,
-//         endDate: 1500004800000,
-//         id: 529
-//       },
-//       {
-//         startDate: 1493006400000,
-//         endDate: 1493956800000,
-//         id: 531
-//       }
-//     ],
-//     skills: [],
-//     certifications: [],
-//     active: true
-//   }
-// ];
+import { Trainer } from '../../model/Trainer';
+
+export const trainers: Trainer[] = [
+  {
+    id: 1,
+    firstName: 'August',
+    lastName: 'Duet',
+    resume: null,
+    unavailabilities: [
+      {
+        startDate: 1502078400000,
+        endDate: 1507867200000,
+        id: 317
+      },
+      {
+        startDate: 1517806800000,
+        endDate: 1523592000000,
+        id: 555
+      },
+      {
+        startDate: 1508731200000,
+        endDate: 1515128400000,
+        id: 412
+      },
+      {
+        startDate: 1495425600000,
+        endDate: 1501214400000,
+        id: 217
+      },
+      {
+        startDate: 1494820800000,
+        endDate: 1501819200000,
+        id: 48
+      },
+      {
+        startDate: 1494820800000,
+        endDate: 1501819200000,
+        id: 46
+      }
+    ],
+    skills: [
+      {
+        id: 4,
+        name: 'AngularJS',
+        active: true
+      },
+      {
+        id: 1,
+        name: 'Core Java',
+        active: true
+      },
+      {
+        id: 2,
+        name: 'JUnit',
+        active: true
+      },
+      {
+        id: 3,
+        name: 'Spring',
+        active: true
+      },
+      {
+        id: 50,
+        name: 'Java Servlets',
+        active: true
+      },
+      {
+        id: 51,
+        name: 'JSP',
+        active: true
+      },
+      {
+        id: 48,
+        name: 'JDBC',
+        active: true
+      },
+      {
+        id: 49,
+        name: 'HTML',
+        active: true
+      },
+      {
+        id: 52,
+        name: 'Freemarker',
+        active: true
+      },
+      {
+        id: 53,
+        name: 'CSS',
+        active: true
+      },
+      {
+        id: 54,
+        name: 'jQuery',
+        active: true
+      },
+      {
+        id: 55,
+        name: 'Hibernate',
+        active: true
+      },
+      {
+        id: 56,
+        name: 'REST',
+        active: true
+      },
+      {
+        id: 57,
+        name: 'SOAP',
+        active: true
+      },
+      {
+        id: 70,
+        name: 'Python',
+        active: true
+      }
+    ],
+    certifications: [],
+    active: true,
+    preferredLocation: {
+      id: 1,
+      name: 'Revature HQ',
+      city: 'Reston',
+      state: 'VA',
+      active: true,
+      buildings: []
+    }
+  },
+  {
+    id: 2,
+    firstName: 'Fred',
+    lastName: 'Belotte',
+    resume: null,
+    unavailabilities: [
+      {
+        startDate: 1494216000000,
+        endDate: 1495166400000,
+        id: 210
+      },
+      {
+        startDate: 1512363600000,
+        endDate: 1518757200000,
+        id: 489
+      },
+      {
+        startDate: 1520827200000,
+        endDate: 1526616000000,
+        id: 610
+      },
+      {
+        startDate: 1503288000000,
+        endDate: 1509681600000,
+        id: 387
+      },
+      {
+        startDate: 1495425600000,
+        endDate: 1501214400000,
+        id: 232
+      }
+    ],
+    skills: [],
+    certifications: [],
+    active: true,
+    preferredLocation: {
+      id: 1,
+      name: 'Revature HQ',
+      city: 'Reston',
+      state: 'VA',
+      active: true,
+      buildings: []
+    }
+  },
+  {
+    id: 3,
+    firstName: 'Joe',
+    lastName: 'Kirkbride',
+    resume: null,
+    unavailabilities: [
+      {
+        startDate: 1498449600000,
+        endDate: 1504238400000,
+        id: 237
+      }
+    ],
+    skills: [],
+    certifications: [],
+    active: false,
+    preferredLocation: {
+      id: 1,
+      name: 'Revature HQ',
+      city: 'Reston',
+      state: 'VA',
+      active: true,
+      buildings: []
+    }
+  },
+  {
+    id: 11,
+    firstName: 'Nicholas',
+    lastName: 'Jurczak',
+    resume: null,
+    unavailabilities: [
+      {
+        startDate: 1507521600000,
+        endDate: 1513314000000,
+        id: 473
+      },
+      {
+        startDate: 1519621200000,
+        endDate: 1525406400000,
+        id: 553
+      },
+      {
+        startDate: 1499659200000,
+        endDate: 1505448000000,
+        id: 257
+      }
+    ],
+    skills: [],
+    certifications: [],
+    active: true,
+    preferredLocation: {
+      id: 1,
+      name: 'Revature HQ',
+      city: 'Reston',
+      state: 'VA',
+      active: true,
+      buildings: []
+    }
+  },
+  {
+    id: 7,
+    firstName: 'Yuvi',
+    lastName: 'Damodaran',
+    resume: null,
+    unavailabilities: [
+      {
+        startDate: 1522641600000,
+        endDate: 1528430400000,
+        id: 615
+      },
+      {
+        startDate: 1499054400000,
+        endDate: 1504843200000,
+        id: 272
+      },
+      {
+        startDate: 1494216000000,
+        endDate: 1501214400000,
+        id: 32
+      },
+      {
+        startDate: 1493006400000,
+        endDate: 1495166400000,
+        id: 67
+      },
+      {
+        startDate: 1512968400000,
+        endDate: 1519362000000,
+        id: 492
+      },
+      {
+        startDate: 1505102400000,
+        endDate: 1510894800000,
+        id: 527
+      },
+      {
+        startDate: 1494216000000,
+        endDate: 1500004800000,
+        id: 529
+      },
+      {
+        startDate: 1493006400000,
+        endDate: 1493956800000,
+        id: 531
+      }
+    ],
+    skills: [],
+    certifications: [],
+    active: true,
+    preferredLocation: {
+      id: 1,
+      name: 'Revature HQ',
+      city: 'Reston',
+      state: 'VA',
+      active: true,
+      buildings: []
+    }
+  }
+];
